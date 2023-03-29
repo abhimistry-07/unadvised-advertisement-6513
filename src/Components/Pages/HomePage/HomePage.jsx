a762c52775fd28eba115c0382c3ee5bd1abb073d
Your Name <you@example.com>
2023-03-29 21:30:33 +0530

    navbar added

---
+++ src/Components/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import UpperNavBar from "./UpperNavbar";
+import MiddleNavbar from "./MiddleNavbar";
+
+function HomePage() {
+  return (
+    <nav>
+      <UpperNavBar />
+      <MiddleNavbar />
+    </nav>
+  );
+}
+
+export default HomePage;
---
+++ src/Components/Pages/HomePage/UpperNavbar.jsx
@@ -0,0 +1,209 @@
+import {
+  Box,
+  Flex,
+  Text,
+  Icon,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Grid,
+} from "@chakra-ui/react";
+
+import { ChevronDownIcon, ChatIcon, PhoneIcon } from "@chakra-ui/icons";
+import { useState } from "react";
+import {
+  BsTicketDetailed,
+  BsQuestionCircle,
+  BsPhoneFill,
+} from "react-icons/bs";
+
+function UpperNavBar() {
+  // const [language, setLanguage] = useState(false);
+  // const [support, setSupport] = useState(false);
+  // const [downloadApp, setDownloadApp] = useState(false);
+  //   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Box className="head_top" bg="#06f">
+      <Flex ml="800px">
+        <Menu
+          // isOpen={downloadApp}
+          as={Button}
+          //   onClose={() => setDownloadApp(false)}
+        >
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            colorScheme="#06f"
+            // onMouseEnter={() => setDownloadApp(true)}
+            // onMouseLeave={() => setDownloadApp(false)}
+          >
+            <Flex>
+              <BsPhoneFill />
+              <Text marginLeft="5px">Save $50 with App</Text>
+            </Flex>
+          </MenuButton>
+          <MenuList>
+            <Card maxW="300px">
+              <Text>
+                Download app to get up to 70% off app-only deals and coupons
+                worth $50
+              </Text>
+              <Grid
+                templateColumns="repeat(2, 1fr)"
+
+                // direction={{ base: "column", sm: "row" }}
+                // overflow="hidden"
+                // variant="outline"
+              >
+                <Image src="https://content1.geekbuying.com/V1.4/en/images/index_images/android_app.png" />
+                <Box>
+                  <Image
+                    p="1"
+                    src="https://content1.geekbuying.com/V1.4/en/images/index_images/app_store.jpg"
+                  />
+                  <Image
+                    p="1"
+                    src="https://content1.geekbuying.com/V1.4/en/images/index_images/google_play.jpg"
+                  />
+                  <Image
+                    p="1"
+                    src="https://content1.geekbuying.com/V1.4/en/images/index_images/gallery.jpg"
+                  />
+                </Box>
+              </Grid>
+              <Text>Go to mobile site</Text>
+            </Card>
+          </MenuList>
+        </Menu>
+        <Menu
+          // isOpen={language}
+          //   isOpen={isOpen}
+          as={Button}
+          //   onClose={() => setLanguage(false)}
+          //   onMouseEnter={() => setLanguage(true)}
+          //   onMouseLeave={() => setLanguage(false)}
+        >
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            colorScheme="#06f"
+            // onMouseEnter={onOpen}
+            // onMouseLeave={onClose}
+          >
+            English
+          </MenuButton>
+          <MenuList
+            minWidth="100px"
+            // onMouseEnter={onOpen}
+            // onMouseLeave={onClose}
+          >
+            <MenuItem>English</MenuItem>
+            <MenuItem>Italiano</MenuItem>
+            <MenuItem>Deutsch</MenuItem>
+            <MenuItem>Español</MenuItem>
+            <MenuItem>Français</MenuItem>
+            <MenuItem>Português</MenuItem>
+            <MenuItem>Slovenčina</MenuItem>
+            <MenuItem>Magyar</MenuItem>
+            <MenuItem>Slovenščina</MenuItem>
+            <MenuItem>Lietuvių kalba</MenuItem>
+            <MenuItem>Svenska</MenuItem>
+            <MenuItem>Ελληνικά</MenuItem>
+            <MenuItem>Dansk</MenuItem>
+            <MenuItem>עברית</MenuItem>
+            <MenuItem>Hrvatski jezik</MenuItem>
+            <MenuItem>Polskie</MenuItem>
+            <MenuItem> Limba română</MenuItem>
+            <MenuItem>Čeština</MenuItem>
+            <MenuItem>日本語</MenuItem>
+          </MenuList>
+        </Menu>
+        <Menu
+        //   isOpen={isOpen}
+        // isOpen={support}
+        >
+          <MenuButton
+            as={Button}
+            rightIcon={<ChevronDownIcon />}
+            colorScheme="#06f"
+            // onMouseEnter={() => setSupport(true)}
+            // onMouseLeave={() => setSupport(false)}
+            // onMouseEnter={onOpen}
+            // onMouseLeave={onClose}
+          >
+            Support Center
+          </MenuButton>
+          <MenuList
+            minWidth="100px"
+            // onMouseEnter={onOpen}
+            // onMouseLeave={onClose}
+          >
+            <MenuItem minH="48px">
+              <ChatIcon mr="12px" />
+              <span>Live Chat</span>
+            </MenuItem>
+            <MenuItem minH="40px">
+              <BsTicketDetailed mr="12px" />
+              <Text mr="12px">Ticket</Text>
+            </MenuItem>
+            <MenuItem>
+              <BsQuestionCircle mr="12px" />
+              Help Center
+            </MenuItem>
+            <MenuItem>
+              <PhoneIcon mr="12px" />
+              Call Us
+            </MenuItem>
+          </MenuList>
+        </Menu>
+      </Flex>
+    </Box>
+  );
+}
+
+//   const Navbar = () => {
+//     return (
+//       <Flex
+//         as="nav"
+//         align="center"
+//         justify="space-between"
+//         wrap="wrap"
+//         bg="teal.500"
+//         color="white"
+//         padding="1rem"
+//       >
+//         <Box>
+//           <Button variant="ghost" color="white">
+//             Logo
+//           </Button>
+//         </Box>
+
+//         <Box display={{ base: "none", md: "block" }}>
+//           <Menu>
+//             <MenuButton as={Button} rightIcon="chevron-down">
+//               Dropdown
+//             </MenuButton>
+//             <MenuList>
+//               <MenuItem>Item 1</MenuItem>
+//               <MenuItem>Item 2</MenuItem>
+//               <MenuDivider />
+//               <MenuItem>Item 3</MenuItem>
+//             </MenuList>
+//           </Menu>
+//         </Box>
+//       </Flex>
+//     );
+//   };
+//   export default Navbar;
+
+export default UpperNavBar;
